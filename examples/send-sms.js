4dc8cf95ae9884385942df18db01f2f1627a3627
aktsbot <box.ashishk@gmail.com>
2023-04-25 21:18:44 +0530

    add example for textlocal

--- examples/send-sms.js
+++ examples/send-sms.js
@@ -1,3 +1,28 @@
+import axios from 'axios';
+
 import config from '../app/config.js'
 
-console.log("It works!")
+function sendSMS() {
+  let sender = encodeURIComponent('TXTLCL');
+  let encoded_message = encodeURIComponent('Testing SMS with textlocal.in');
+
+  return axios.post('https://api.textlocal.in/send/', {
+    'apikey': config.sms_api_key,
+    'numbers': '##replace_me##',
+    'message': encoded_message,
+    'sender': sender
+  })
+}
+
+async function start() {
+  try {
+    const resp = await sendSMS();
+    console.log(resp.data);
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+start()
